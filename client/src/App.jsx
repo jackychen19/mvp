03ccbcbc93eaec96f537608f03615657a4f158e5
Jacky Chen <jacky.chen0319@gmail.com>
2021-07-07 17:16:51 -0700

    Created search recipe API route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 const App = () => {
   const [name, setName] = useState('Jacky');
 
+  const search = () => {
+    console.log('searched!');
+    axios('/recipes', {params: {'food': 'kimchi fried rice'}})
+      .then(response => console.log(response.data))
+      .catch(err => console.error(err));
+  };
+
   return (
-    <div>Hello, {name}!</div>
+    <div>
+      <div>Hello, {name}!</div>
+      <button onClick={search}>Test</button>
+    </div>
   );
 };
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+const config = require('./config.js');
 
 const app = express();
 const port = 6969;
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -12,4 +16,25 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
+});
+
+app.get('/recipes', (req, res) => {
+  let query = req.query;
+  console.log('QUERY:', query);
+  axios({
+    url: 'https://api.edamam.com/api/recipes/v2',
+    params: {
+      type: 'public',
+      q: query.food,
+      app_id: config.appID,
+      app_key: config.appKey
+    }
+  })
+    .then(response => {
+      res.status(200).send(response.data);
+    })
+    .catch(err => {
+      console.error(err);
+      res.send(400);
+    });
 });
